2cecdff7f44a836992a2adce290f98b2d0376853
Karen <8099322@qq.com>
2023-06-02 10:52:43 +0800

    🔧 修复(update_zip_code.js)：修复updateURL和downloadURL 修复了updateURL和downloadURL，以便它们指向正确的URL。这将确保脚本在更新时能够正确下载和更新。

--- update_zip_code.js
+++ update_zip_code.js
@@ -5,8 +5,8 @@
 // @author          Karen
 // @namespace       https://github.com/gopkg-dev/amazon_scripts
 // @supportURL      https://github.com/gopkg-dev/amazon_scripts
-// @updateURL       https://github.com/gopkg-dev/amazon_scripts/main/update_zip_code.js
-// @downloadURL     https://github.com/gopkg-dev/amazon_scripts/main/update_zip_code.js
+// @updateURL       https://raw.githubusercontent.com/gopkg-dev/amazon_scripts/main/update_zip_code.js
+// @downloadURL     https://raw.githubusercontent.com/gopkg-dev/amazon_scripts/main/update_zip_code.js
 // @icon            data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" stroke-width="2" fill="none" stroke="currentColor"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
 // @license         GPL-2.0-only
 // @match           https://*.amazon.*
