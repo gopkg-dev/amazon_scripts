e5a55c3e52e7bf28445e875e43f0385443a7f181
Karen <8099322@qq.com>
2023-08-26 10:48:31 +0800

    chore(amazon.user.js): update version number from 0.1.3 to 0.1.4 chore(amazon.user.js): change 'run-at' metadata from 'document-idle' to 'document-end' chore(amazon.user.js): update URL for wasm file from amazoncaptchaV1.3.wasm to amazoncaptchaV1.3.5.wasm The version number is updated to reflect the changes made in the script. The 'run-at' metadata is changed to ensure the script runs at the end of the document. The URL for the wasm file is updated to the latest version, amazoncaptchaV1.3.5.wasm.

--- amazon.user.js
+++ amazon.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            AmazonHelper
 // @description     根据当前打开的亚马逊站点无感自动输入对应邮编,自动输入验证码.
-// @version         0.1.3
+// @version         0.1.4
 // @author          Karen
 // @namespace       https://github.com/gopkg-dev/amazon_scripts
 // @supportURL      https://github.com/gopkg-dev/amazon_scripts
@@ -10,9 +10,8 @@
 // @icon            data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" stroke-width="2" fill="none" stroke="currentColor"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
 // @license         GPL-2.0-only
 // @match           https://*.amazon.*
-// @run-at          document-idle
+// @run-at          document-end
 // @grant           unsafeWindow
-// @run-at          document-idle
 // @require         https://cdn.jsdelivr.net/gh/gopkg-dev/amazon_scripts@main/wasm_exec_tinygo.js
 // ==/UserScript==
 
@@ -48,6 +47,6 @@
     }
 
     // Load the wasm file
-    loadWasm("https://cdn.jsdelivr.net/gh/gopkg-dev/amazon_scripts@main/amazoncaptchaV1.3.wasm").catch(console.error);
+    loadWasm("https://cdn.jsdelivr.net/gh/gopkg-dev/amazon_scripts@main/amazoncaptchaV1.3.5.wasm").catch(console.error);
 
 })();
